c4126e6ff794177582d07438a760ac4c66edc4cc
shenn034 <taurus940430@gmail.com>
2018-11-19 00:03:20 +1100

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by shen on 19/11/18.
+//  Copyright © 2018 shen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
